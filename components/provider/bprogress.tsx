c1c0f9a9a32b166e1c69b06536f3504f06b39d24
KYs <amitkys59@gmail.com>
2026-01-31 02:56:00 +0530

    change bprogress color as theme change

--- components/provider/bprogress.tsx
+++ components/provider/bprogress.tsx
@@ -1,12 +1,23 @@
 'use client';
 
 import { ProgressProvider } from '@bprogress/next/app';
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 const BProgressProviders = ({ children }: { children: React.ReactNode }) => {
+  const { resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const color = mounted && resolvedTheme === 'light' ? '#1F51FF' : 'var(--color-fd-primary)';
+
   return (
     <ProgressProvider
-      height="4px"
-      color="#fffd00"
+      height="3px"
+      color={color}
       options={{ showSpinner: false }}
       shallowRouting
     >
